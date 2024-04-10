183657a8c17262ffbc3e3bff0020e0c310699218
soni kumari <soniindu92@gmail.com>
2024-04-10 16:10:59 +0530

    127:meeting blog

---
+++ src/main/java/com/meetingPractice/MeetingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.meetingPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetingPractice/MeetingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetingPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
